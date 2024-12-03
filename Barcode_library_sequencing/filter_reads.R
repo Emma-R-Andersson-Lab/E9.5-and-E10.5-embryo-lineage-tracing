73bd8d72556049946a0b0b5c3de01825c85480b5
Lenka Belicova <103933009+LenkaBeli@users.noreply.github.com>
2024-12-03 19:01:57 +0100

    Update filter_reads.R

--- Barcode_library_sequencing/filter_reads.R
+++ Barcode_library_sequencing/filter_reads.R
@@ -1,1 +1,14 @@
+# Filter trimmed fastq reads for the base quality (reducing sequencing errors)
 
+# Load the library
+library(seqTools)
+
+# Discard the sequences where more than two bases have quality below 15:
+
+trimFastq("plasmid_lib_50nt.fq.gz",
+          outfile="filtered.fq.gz", discard="discarded.fq.gz",
+          qualDiscard = 15)
+
+## Results:
+#[trimFastq]    24519506 records written to outfile.
+#[trimFastq]    55500238 records written to discard.
